3a8f2172e0e4ccae5e21cdc071f69445a6ab4be6
abel.acosta <abel.acosta@easysoft.com.ec>
2021-10-29 12:23:11 -0500

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,39 +2,62 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  
   {
     path: '',
     redirectTo: 'inicio',
-    pathMatch: 'full',
+    pathMatch: 'full'
   },
   {
     path: 'inicio',
-    loadChildren: () =>
-      import('./pages/inicio/inicio.module').then((m) => m.InicioPageModule),
+    loadChildren: () => import('./pages/inicio/inicio.module').then( m => m.InicioPageModule)
   },
-
   {
     path: 'alert',
-    loadChildren: () =>
-      import('./pages/alert/alert.module').then((m) => m.AlertPageModule),
+    loadChildren: () => import('./pages/alert/alert.module').then( m => m.AlertPageModule)
   },
   {
     path: 'action-sheet',
-    loadChildren: () =>
-      import('./pages/action-sheet/action-sheet.module').then(
-        (m) => m.ActionSheetPageModule
-      ),
+    loadChildren: () => import('./pages/action-sheet/action-sheet.module').then( m => m.ActionSheetPageModule)
   },
   {
-    path: 'avatar',
-    loadChildren: () => import('./pages/avatar/avatar.module').then( m => m.AvatarPageModule)
+    path: 'badge',
+    loadChildren: () => import('./pages/badge/badge.module').then( m => m.BadgePageModule)
+  },
+  {
+    path: 'buttons',
+    loadChildren: () => import('./pages/buttons/buttons.module').then( m => m.ButtonsPageModule)
+  },
+  {
+    path: 'card',
+    loadChildren: () => import('./pages/card/card.module').then( m => m.CardPageModule)
+  },
+  {
+    path: 'chip',
+    loadChildren: () => import('./pages/chip/chip.module').then( m => m.ChipPageModule)
+  },
+  {
+    path: 'date-time',
+    loadChildren: () => import('./pages/date-time/date-time.module').then( m => m.DateTimePageModule)
+  },
+  {
+    path: 'picker',
+    loadChildren: () => import('./pages/picker/picker.module').then( m => m.PickerPageModule)
+  },
+  {
+    path: 'fab',
+    loadChildren: () => import('./pages/fab/fab.module').then( m => m.FabPageModule)
+  },
+  {
+    path: 'checkbox',
+    loadChildren: () => import('./pages/checkbox/checkbox.module').then( m => m.CheckboxPageModule)
   },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
   ],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
